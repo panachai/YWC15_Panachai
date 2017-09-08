72ecf56c86622612cd077f86803fc48823cbbe90
Panachai <panachai.ny@gmail.com>
2017-09-08 16:39:46 +0700

    Logic CardComplete

--- YWC15/Controllers/HomeController.cs
+++ YWC15/Controllers/HomeController.cs
@@ -6,7 +6,53 @@ using System.Web.Mvc;
 
 namespace YWC15.Controllers {
     public class HomeController : Controller {
-        public ActionResult Index() {
+
+        //string[,] CardALL = new string[13, 4] {{'2', '3', '4', '5', '6', '7', '8', '9', '0', 'J','Q', 'K', 'A'},
+        //                             {"d","e","f"},
+        //                             {"g","h","i"} };
+
+        //'2', '3', '4', '5', '6', '7', '8', '9', '0', 'J','Q', 'K', 'A'
+        //('C', 'D', 'H', 'S')
+
+        string[] CardType = { "C", "D", "H", "S" };
+        string[] CardNumber = { "2", "3", "4", "5", "6", "7", "8", "9", "0", "J", "Q", "K", "A" };
+      
+
+        public ActionResult Index(string cardnumber) {
+            int n;
+
+            if (cardnumber != null) {
+
+                if (int.TryParse(cardnumber, out n)) {
+                    int type = 0;
+                    int card = 0;
+                    int count = 0;
+
+                    if (n <= 51 && n >= 0) {
+                        while (n > 0) {
+                            card++;
+                            count++;
+                            n--;
+
+                            if (count == 13) {
+                                type++;
+                                count = 0;
+                            }
+                        }
+                        string result = CardNumber[count] + CardType[type];
+
+                        ViewBag.CardResult = result;
+                        return View();
+                    } else {
+                        ViewBag.CardResult = "ห้ามกรอกเกิน 52 ใบ (0-51)";
+                        return View();
+                    }
+                } else {
+                    ViewBag.CardResult = "โปรกรอกตัวเลข";
+                    return View();
+                }
+            }
+            ViewBag.CardResult = "โปรดกรอกการ์ดที่ต้องการ";
             return View();
         }
 
